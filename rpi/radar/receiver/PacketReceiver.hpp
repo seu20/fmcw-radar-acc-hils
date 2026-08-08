f5d9f97fc85482ac222f9244d28c5c2fa5b812e7
John <johnhong2000107@naver.com>
2026-08-08 19:24:16 +0900

    Thread Safe Queue 추가: 동시성 부여한 queue

--- rpi/radar/receiver/PacketReceiver.hpp
+++ rpi/radar/receiver/PacketReceiver.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include "protocol.hpp"
+#include <queue>
+
+// UDP 로 packet 받고 Packet객체의 queue 에 push
+class PacketReceiver {
+private:
+    queue <RadarPacket>& packet_queue_;
+
+public:
+    UdpReceiver(
+        ThreadSafeQueue<RadarPacket>& queue
+    )
+        : packet_queue_(queue)
+    {
+    }
+};
+};
